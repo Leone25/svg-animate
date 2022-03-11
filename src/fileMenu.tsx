2735ce66ab4ccd5005de03c663073d6cce8da38c
Enrico Franciscono <github@enrico.wtf>
2022-03-11 11:09:21 +0100

    filemenu tabs

--- src/fileMenu.tsx
+++ src/fileMenu.tsx
@@ -1,16 +1,35 @@
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { ArrowLeft32Regular } from "@fluentui/react-icons";
+
 
 function FileMenu({ closeCallback, show } : {
     closeCallback: () => void;
     show: boolean;
 }) {
     return (
-        <div className={"filemenu" + (show ? " open" : "")}>
-            <div className="left">
-                <div onClick={closeCallback}>return</div>
+        <Tabs className={"filemenu" + (show ? " open" : "")}>
+            <TabList>
+                <div onClick={closeCallback}><ArrowLeft32Regular /></div>
+                <Tab>one</Tab>
+                <Tab>two</Tab>
+                <Tab>three</Tab>
+                <Tab>four</Tab>
+            </TabList>
+            <div>
+                <TabPanel>
+                    fileMenu1
+                </TabPanel>
+                <TabPanel>
+                    fileMenu2
+                </TabPanel>
+                <TabPanel>
+                    fileMenu3
+                </TabPanel>
+                <TabPanel>
+                    fileMenu4
+                </TabPanel>
             </div>
-            
-            fileMenu
-        </div>
+        </Tabs>
     );
   }
   
