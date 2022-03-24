0a9eecfe07df1bfbc2fe37148ead35c4970dea1d
Enrico Franciscono <github@enrico.wtf>
2022-03-24 08:51:12 +0100

    continue work on resizable area, defenetly still a WIP

--- src/components/resizableArea/resizableArea.tsx
+++ src/components/resizableArea/resizableArea.tsx
@@ -1,10 +1,28 @@
-export function ResizableArea({children} : {children: React.ReactChild[]}) {
+import { useState } from "react";
+
+export function ResizableArea({
+    children, 
+    initialSizes
+} : {
+    children: React.ReactChild[],
+    initialSizes?: number[]
+}) {
     console.log(children);
-    return children && children.reduce((acc: React.ReactNode, child: React.ReactNode, i: Number, arr: React.ReactChild[] = []) => {
-        return (<>
-            {acc} 
-            {child}
-            {i === (arr || []).length - 1 || (<div>spacer</div>)}
-        </>);
-    }, (<></>));
+
+    let [sizes, setSizes] = useState(initialSizes || new Array(children.length-1).fill(1/(children.length-1)));
+    
+    return (<div style={{
+            display:'grid',
+            height:'100%',
+            gridTemplateColumns: '1fr',
+            gridTemplateRows: sizes.reduce((acc: String, child: number, i: number, arr: number[] = []) => acc + String(child*100) + '% 10px ', '') + '1fr'
+        }}>
+        {children && children.reduce((acc: React.ReactNode, child: React.ReactNode, i: Number, arr: React.ReactChild[] = []) => {
+            return (<>
+                {acc} 
+                {child}
+                {i === (arr || []).length - 1 || (<div style={{background: 'red'}}></div>)}
+            </>);
+        }, (<></>))}
+    </div>)
 }
