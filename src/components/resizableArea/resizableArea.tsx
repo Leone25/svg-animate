2e59913a1de488a4f295ec1440e952762e7211d5
Enrico Franciscono <github@enrico.wtf>
2022-03-25 11:32:27 +0100

    drag resizing working, hopefully bug free

--- src/components/resizableArea/resizableArea.tsx
+++ src/components/resizableArea/resizableArea.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 export function ResizableArea({
     children, 
@@ -7,16 +7,32 @@ export function ResizableArea({
     children: [React.ReactChild, React.ReactChild],
     initialSizes?: number
 }) {
+    let containerRef = React.useRef<HTMLDivElement>(null);
     let [size, setSize] = useState(initialSizes || 0.5);
+    let [isDragging, setIsDragging] = useState(false);
+
+    let startDrag = useCallback((e: React.MouseEvent) => {
+        setIsDragging(true);
+    }, [setIsDragging]);
+    let endDrag = useCallback((e: React.MouseEvent) => {
+        setIsDragging(false);
+    }, [setIsDragging]);
+    let drag = useCallback((e: React.MouseEvent) => {
+        if (!isDragging) return;
+        setSize(Math.max(0, Math.min(1, (e.clientY - (containerRef.current?.offsetTop || 0)) / (containerRef.current?.clientHeight || 1))));
+    }, [setSize, isDragging, containerRef]);
     
     return (<div style={{
-            display:'grid',
-            height:'100%',
-            gridTemplateColumns: '1fr',
-            gridTemplateRows: `calc(${size*100}% - 5px) 10px 1fr`
-        }}>
+                display:'grid',
+                height:'100%',
+                gridTemplateColumns: '1fr',
+                gridTemplateRows: `calc(${size*100}% - 5px) 10px 1fr`
+            }}
+            ref={containerRef}
+            onMouseUp={endDrag} onMouseMove={drag}
+        >
         {children[0]}
-        <div style={{background:'red'}}></div>
+        <div style={{background:'red', cursor:'row-resize'}} onMouseDown={startDrag}></div>
         {children[1]}
     </div>)
 }
